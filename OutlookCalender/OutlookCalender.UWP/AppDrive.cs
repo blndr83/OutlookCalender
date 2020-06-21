1c32689ce596baf9eeaae50ec8fe56fd2b66f628
Daniel <42676493+blndr83@users.noreply.github.com>
2020-06-21 18:37:50 +0200

    implemented AppDrive on uwp
    implemented AppDrive on uwp

--- OutlookCalender/OutlookCalender.UWP/AppDrive.cs
+++ OutlookCalender/OutlookCalender.UWP/AppDrive.cs
@@ -1,12 +1,19 @@
 ﻿using FileSystemInterfaces;
 using System;
+using System.IO;
+using Windows.Storage;
 
 namespace OutlookCalender.UWP
 {
     public class AppDrive : IAppDrive
     {
-        public string DriveName => throw new NotImplementedException();
+        public string DriveName { get; }
 
-        public long FreeSpace => throw new NotImplementedException();
+        public long FreeSpace => Convert.ToInt64(StorageFolder.GetFolderFromPathAsync(AppDomain.CurrentDomain.BaseDirectory).AsTask().Result.Properties.RetrievePropertiesAsync(new[] { "System.FreeSpace"}).AsTask().Result["System.FreeSpace"]);
+        
+        public AppDrive()
+        {
+            DriveName = Directory.GetDirectoryRoot(AppDomain.CurrentDomain.BaseDirectory);
+        }
     }
 }
